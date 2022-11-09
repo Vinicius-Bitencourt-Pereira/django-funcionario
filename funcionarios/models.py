589f03645e8bec4f3c10f02922d726bc08b119dd
Vinícius Bitencourt Pereira <viniciusbitencourtpereira@gmail.com>
2022-11-09 19:47:45 -0300

    DB Postgre e models Departamento e Funcionario

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Departamento(models.Model):
+    nome = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+
+    def __str__(self):
+        return self.nome
+
+
+class Funcionario(models.Model):
+    nome = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    telefone = models.CharField(
+        max_length=25,
+        null=False,
+        blank=False
+    )
+
+    email = models.EmailField(
+        null=False,
+        blank=True
+    )
+
+    salario = models.DecimalField(
+        max_digits=8, 
+        decimal_places=2,
+        null=False,
+        blank=False
+    )
+
+    data_contratacao = models.DateField(
+        null=False,
+        blank=False
+    )
+
+    departamento = models.ForeignKey(
+        Departamento, 
+        on_delete=models.DO_NOTHING
+    )
+
+    def __str__(self):
+        return self.nome
--- projeto_funcionarios/settings.py
+++ projeto_funcionarios/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'projeto_funcionarios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'funcionarios',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
