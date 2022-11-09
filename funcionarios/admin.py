a88e9de66d7e34ff9b35270fd551f9aecfa5f6a0
Vinícius Bitencourt Pereira <viniciusbitencourtpereira@gmail.com>
2022-11-09 19:55:21 -0300

    Admin

--- funcionarios/admin.py
+++ funcionarios/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Departamento, Funcionario
 
-# Register your models here.
+
+@admin.register(Departamento)
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+    list_display_links = ['nome']
+    list_per_page = 10
+    search_fields = ['nome']
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'telefone', 'email', 'salario', 'data_contratacao', 'departamento']
+    list_display_links = ['nome']
+    ordering = ['nome']
+    list_per_page = 10
+    search_fields = ['nome', 'departamento']
